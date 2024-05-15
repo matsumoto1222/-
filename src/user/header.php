921edf03560021a528eb6ded5008122aa342861d
yusei888 <2201146@s.asojuku.ac.jp>
2024-05-15 10:47:31 +0900

    Update header.php

--- src/user/header.php
+++ src/user/header.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="UTF-8">
 <title>
-    <img class="rogo" src="img/AGB1.png" alt="ロゴ写真" width="129vw" height="110">
+    <img class="rogo" src="img/AGB.png" alt="ロゴ写真" width="129vw" height="110">
 </title>
 </head>
 <body>
