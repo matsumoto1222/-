ec17bd2e5835cf44b3c0fa9c3bf73207a329ffc3
yusei888 <2201146@s.asojuku.ac.jp>
2024-05-20 11:34:26 +0900

    Update db-connect.php

--- src/user/db-connect.php
+++ src/user/db-connect.php
@@ -1,10 +1,10 @@
 <?php
-    const SERVER ='mysql214.phy.lolipop.lan';
-    const DBNAME ='LAA1516823-shop';
-    const USER ='LAA1516823';
-    const PASS ='Pass';
+    const SERVER ='mysql304.phy.lolipop.lan';
+    const DBNAME ='LAA1516804-budou';
+    const USER ='LAA1516804';
+    const PASS ='pass1109';
 
     $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
 ?>
-//前のやつ↑
+
 
